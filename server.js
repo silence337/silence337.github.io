c1d1079d3901abb3c94093930d5a1a9c9e15ba11
silence337 <silence337@naver.com>
2021-10-26 16:25:45 +0900

    Updates

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 app.use(express.static(__dirname));
-app.get("*", function(req, res) {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 function handleListenLog() {
-  console.log("Server Starting...");
+  console.log('Server Starting...');
 }
 app.listen(9000, handleListenLog);
